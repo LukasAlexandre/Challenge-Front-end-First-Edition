738e8de7bfd58bed4a6858f7ed68c09649f2251f
lukasalexandre <larosa2019@hotmail.com>
2024-04-23 16:58:42 -0300

    Alteração e funcionalidade Highlight e thema code zone

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+const linguagem = document.querySelector('#linguagem')
+const areaDoCodigo = document.querySelector('.codigo-wrapper')
+const botao = document.querySelector('.button-highlight')
 const menu = document.querySelector('.menu');
 const menuIcon = document.querySelector('.menu-icon');
 const menuIconX = document.querySelector('.menu-icon-x');
+let tela = window.innerWidth;
 
 
 function abrirJanela() {
-    if (menu.style.display === 'block') {
+    let estiloMenu = window.getComputedStyle(menu);
+
+    if (estiloMenu.display === 'block') {
         menu.style.display = 'none';
-        menuIcon.style.display = 'block'
-        menuIconX.style.display = 'none'
-    } else {
+        menuIcon.style.display = 'block';
+        menuIconX.style.display = 'none';
+    } else if (estiloMenu.display === 'none' && tela < 1028) {
         menu.style.display = 'block';
-        menuIcon.style.display = 'none'
-        menuIconX.style.display = 'block'
-    }
+        menuIcon.style.display = 'none';
+        menuIconX.style.display = 'block';
+    };
 };
 
+function aplicarHighlight() {
+    const codigo = areaDoCodigo.innerText; // Use innerText em vez de textContent
+    areaDoCodigo.innerHTML = `
+    <div class="span-ball">
+        <div class="ball-one"></div>
+        <div class="ball-two"></div>
+        <div class="ball-three"></div>
+    </div>
+        <code class="preview hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de Código"></code>
+</div>`;
+    areaDoCodigo.querySelector('code').textContent = codigo; // Use innerText em vez de textContent
+    hljs.highlightElement(areaDoCodigo.querySelector('code'));
+}
+botao.addEventListener('click', () => {
+    aplicarHighlight();
+})
